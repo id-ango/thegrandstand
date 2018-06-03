69abb75a3ddf5712f820adbd5b091de66f6c2b6a
Edward Edwin Aldrin Angouw <globalcom69@yahoo.com>
2018-06-03 09:42:04 +0700

    Add project files.

---
+++ GlobalSoft/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GlobalSoft.MvcApplication" Language="C#" %>
---
+++ GlobalSoft/Models/ApartmentModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GlobalSoft.Models
+{
+    public class AptCategorie
+    {
+        
+        public int Id { get; set; }
+
+        [Display(Name = "Kategori"),Required]
+        [StringLength(20)]
+        public string Categorie { get; set; }
+
+        public decimal Size { get; set; }
+    }
+
+    public class AptType
+    {
+        public int Id { get; set; }
+
+        [StringLength(20),Required]
+        public string Tipe { get; set; }
+    }
+}
