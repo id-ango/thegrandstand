e26a7b7611b488a7c80b2b58213c5ab12d100551
Edward Edwin Aldrin Angouw <globalcom69@yahoo.com>
2018-06-03 15:44:06 +0700

    Add Model Apartment

--- GlobalSoft/Models/ApartmentModels.cs
+++ GlobalSoft/Models/ApartmentModels.cs
@@ -6,7 +6,23 @@ using System.Web;
 
 namespace GlobalSoft.Models
 {
-    public class AptCategorie
+
+    public class AptType                // Tipe = 1BR,2BR    not Mapped
+    {
+        public int Id { get; set; }
+
+        [StringLength(20), Required]
+        public string Tipe { get; set; }
+    }
+
+    public class AptStatus              // Status , 0 Available, 1 Hold, 5 Sold    not Mapped
+    {
+        public int Id { get; set; }
+        public String Status { get; set; }
+    }
+
+
+    public class AptCategorie       //Kategori = Emerald
     {
         
         public int Id { get; set; }
@@ -17,12 +33,19 @@ namespace GlobalSoft.Models
 
         public decimal Size { get; set; }
     }
-
-    public class AptType
+   
+    public class AptUnit                                // Unit Apartment
     {
         public int Id { get; set; }
 
-        [StringLength(20),Required]
-        public string Tipe { get; set; }
+        [Display(Name = "Unit"),Required]
+        public int UnitNo { get; set; }
+
+        public int CategorieId { get; set; }
+        public virtual AptCategorie AptCategorie { get; set; }
+
+        public int StatusId { get; set; }
+        public virtual AptStatus AptStatus { get; set; }
+
     }
 }
